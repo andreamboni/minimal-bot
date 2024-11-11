2189a0939549a31b4ed4b079f1a1196edd814c04
Andre Amboni <amboni.andre@gmail.com>
2024-11-10 21:24:30 -0300

    adding test message

--- src/main/java/com/minimal_not_a_bot/MinimalNotABotApplication.java
+++ src/main/java/com/minimal_not_a_bot/MinimalNotABotApplication.java
@@ -5,7 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import com.minimal_not_a_bot.bot.Bot;
 import com.minimal_not_a_bot.service.MessageService;
 
 @SpringBootApplication
@@ -14,13 +18,28 @@ public class MinimalNotABotApplication {
 	@Autowired
 	private MessageService messageService;
 
+	@Autowired
+	private Bot bot;
+
 	public static void main(String[] args) {
 		SpringApplication.run(MinimalNotABotApplication.class, args);
 	}
 
 	@EventListener(ApplicationReadyEvent.class)
 	private void sendMessage() {
-		messageService.sendMessage();
+		TelegramBotsApi botsApi;
+		try {
+			botsApi = new TelegramBotsApi(DefaultBotSession.class);
+			botsApi.registerBot(bot);
+
+			bot.sendText(5186470566L, "Mensagem teste", "HTML");
+
+		} catch (TelegramApiException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// messageService.sendMessage();
 	}
 
 }
